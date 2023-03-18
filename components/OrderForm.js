dd493f019484b04dd2e8464fce8382c8e0cec1eb
stanken_a <artyemst3@mail.ru>
2023-03-18 14:25:51 +0300

    feat: move OrderForm to components

---
+++ components/OrderForm.js
@@ -0,0 +1,41 @@
+export default class form {
+  constructor(formName) {
+    this._form = document.forms[formName];
+    this._orderBtn = document.querySelector('.order__button');
+    this._orderBtnTxt = this._orderBtn.querySelector('.order__button-txt');
+    this._orderArrow = this._orderBtn.querySelector('.order__arrow');
+
+    this._orderFileInput = this._form.querySelector('.order__input_type_file');
+    this._orderFileName = this._form.querySelector('.order__file-name');
+  }
+
+  _toggleFormOpen() {
+    this._orderArrow.classList.toggle('order__arrow_opened');
+    this._form.classList.toggle('order__form_closed');
+  }
+
+  _controlFormState() {
+    if (this._form.classList.contains('order__form_closed')) {
+      this._form.style.height = '0px';
+      this._orderBtnTxt.textContent = 'раскрыть форму';
+    } else {
+      this._form.style.height = `${this._form.scrollHeight}px`;
+      this._orderBtnTxt.textContent = 'скрыть форму';
+    }
+  }
+
+  _setFileName() {
+    this._orderFileName.textContent = this._orderFileInput.files[0].name;
+  }
+
+  setEventListeners() {
+    this._orderBtn.addEventListener('click', () => {
+      this._toggleFormOpen();
+      this._controlFormState();
+    })
+
+    this._orderFileInput.addEventListener('change', () => {
+      this._setFileName();
+    })
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,8 @@
 import PopupWithImage from "../components/PopupWithImage.js";
+import OrderForm from "../components/OrderForm.js";
 
-const orderBtn = document.querySelector('.order__button');
-const orderBtnTxt = orderBtn.querySelector('.order__button-txt');
-const orderArrow = document.querySelector('.order__arrow');
-const orderForm = document.forms['order'];
-
-const orderFileInput = orderForm.querySelector('.order__input_type_file');
-const orderFileName = orderForm.querySelector('.order__file-name');
-
-orderBtn.addEventListener('click', () => {
-  orderArrow.classList.toggle('order__arrow_opened');
-  orderForm.classList.toggle('order__form_closed');
-
-  if (orderForm.classList.contains('order__form_closed')) {
-    orderForm.style.height = '0px';
-    orderBtnTxt.textContent = 'раскрыть форму';
-  } else {
-    orderForm.style.height = `${orderForm.scrollHeight}px`;
-    orderBtnTxt.textContent = 'скрыть форму';
-  }
-});
-
-orderFileInput.addEventListener('change', () => {
-  orderFileName.textContent = orderFileInput.files[0].name;
-});
+const orderForm = new OrderForm('order');
+orderForm.setEventListeners();
 
 const swiperHero = new Swiper('.hero__swiper', {
   slidesPerView: 1,
@@ -138,6 +117,7 @@ function changeHeight(container, selectorOpened) {
 }
 
 menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('header__menu-btn_opened');
   headerCatalog.classList.toggle('catalog_opened');
   headerMenu.classList.toggle('menu__nav_opened');
   headerInfo.classList.toggle('header__info-container_opened');
