0fe4a137637c2ef0b09d92697484dd81297873f9
stanken_a <artyemst3@mail.ru>
2023-03-19 20:28:18 +0300

    feat: add production page

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,3 @@
 export const certificatesImages = document.querySelectorAll('.certificates__img');
 export const documentsImages = document.querySelectorAll('.documents__img');
+export const productionImages = document.querySelectorAll('.production__img')
